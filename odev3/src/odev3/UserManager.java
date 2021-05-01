a243e4bcf8c731f2082cb9f999be8c37636c0d02
example <email@example.com>
2021-05-01 20:02:10 +0300

    User Manager düzenlendi

--- odev3/src/odev3/UserManager.java
+++ odev3/src/odev3/UserManager.java
@@ -1,6 +1,7 @@
 package odev3;
 
 public class UserManager {
+	
 	public void add(User user) {
 		System.out.println("First name:" + user.getFirstName() );
 		System.out.println("Last name:" + user.getLastName());
@@ -12,6 +13,13 @@ public class UserManager {
 			add(user);
 		}
 	}
+	
+
+	
+	
+	
+	
+	
 	
 
 }
